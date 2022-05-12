088ed96ecfce0262895f99aa7aaa7f5d8007d793
andrey <andreytorrente@gmail.com>
2022-05-12 16:05:52 -0600

    added catch to fetch

--- src/helpers/Fetch.js
+++ src/helpers/Fetch.js
@@ -1,5 +1,11 @@
-export const dataRequest  = (url, callback) => {  
-    fetch(url)
-    .then(response => response.json())
-    .then(data => callback(data))
-}
+export const dataRequest = (url, callback) => {
+  fetch(url)
+    .then((response) =>
+      response.ok ? response.json() : Promise.reject(response)
+    )
+    .then((data) => callback(data))
+    .catch(() => {
+      document.querySelector('.slider-container').style.color='white'
+      document.querySelector('.slider-container').innerHTML = "ERROR ON DATABASE"
+    });
+};
