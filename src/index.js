cd5ce3b9ea13c6c363ad66658fa1422f34efcc9e
andrey <andreytorrente@gmail.com>
2022-05-16 17:19:28 -0600

    add bootstrap to index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "./main.scss";
+require('bootstrap-icons/font/bootstrap-icons.scss');
 import { App } from "./App.js";
 
-document.addEventListener("DOMContentLoaded", App);window.addEventListener("hashchange", App);
+document.addEventListener("DOMContentLoaded", App);
 window.addEventListener("hashchange", App);
