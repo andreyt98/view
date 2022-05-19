2bdd75c9d43d6d40a22c9188b869f5e56549ab4f
andrey <andreytorrente@gmail.com>
2022-05-18 23:43:26 -0600

    change file and function name to fetch data

---
+++ src/helpers/fetchData.js
@@ -0,0 +1,9 @@
+export const fetchData = (url, callback) => {
+  fetch(url)
+    .then((response) => (response.ok ? response.json() : Promise.reject(response)))
+    .then((data) => callback(data))
+
+    .catch((error) => {
+      console.log(error);
+    });
+};
