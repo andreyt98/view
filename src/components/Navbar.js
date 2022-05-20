8e61884c0709ddd63ca16c06d6f049700cfd2c47
andrey <andreytorrente@gmail.com>
2022-05-19 21:31:35 -0600

    added search bar to nav and show results

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,70 @@
+import { fetchData } from "../helpers/fetchData.js";
 import { logo, search } from "../helpers/icons";
-
+import { Input } from "./Input.js";
 export const Navbar = () => {
   const $navBar = document.createElement("NAV");
   $navBar.classList.add("nav");
-  $navBar.innerHTML = `<a href="#home" class="logo"> ${logo} </a>
+  $navBar.innerHTML = `
+  
+    <a href="#home" class="logo"> ${logo} </a>
 
-  <ul class="links">   
-    <li><a href="#movies"> Movies</a> </li>
-    <li><a href="#tv-shows"> TV Shows</a> </li>    
-  </ul>   
+    <ul class="links">   
+      <li><a href="#movies"> Movies</a> </li>
+      <li><a href="#tv-shows"> TV Shows</a> </li>    
+    </ul>   
 
-  ${search}`;
+    <form class="search-form"> 
+    ${Input("search", "Search something", search).innerHTML}
+    </form>
+    
+    <div class="results-container"></div>
+    
+    `;
 
   window.addEventListener("scroll", () => {
     $navBar.classList.toggle("color-on-scroll", window.scrollY > 0);
   });
 
-  document.addEventListener("click", (e) => {
-    if (e.target.classList.contains("search-btn")) {
-      window.location.hash = `search`;
+  document.addEventListener("keyup", (e) => {
+    if (e.target.matches(".search-form input")) {
+      const $resultsContainer = document.querySelector(".results-container");
+      $resultsContainer.style.display = "flex";
+
+      let query = document.querySelector(".search-form input").value;
+      if (!query) $resultsContainer.style.display = "none";
+
+      $resultsContainer.innerHTML = "";
+      fetchData(
+        `https://api.themoviedb.org/3/search/multi?api_key=${process.env.API_K}&language=en-US&query=${query}&page=1&include_adult=false`,
+
+        (data) => {
+          if (data.results.length <= 0)
+            $resultsContainer.innerHTML = `There are no results for "${query}"`;
+
+          data.results.forEach((element) => {
+            const { poster_path, media_type, release_date, first_air_date } = element;
+            const resultName = element.name || element.original_title || element.original_name;
+
+            if ((poster_path && resultName && media_type === "movie") || media_type === "tv") {
+              const $resultItem = document.createElement("DIV");
+              $resultItem.classList.add("result-item");
+
+              const $resultName = document.createElement("P");
+              $resultName.textContent = `${resultName}`;
+
+              const $resultImg = document.createElement("IMG");
+              $resultImg.src = `https://image.tmdb.org/t/p/w500${poster_path}`;
+
+              const $resultYear = document.createElement("P");
+              $resultYear.textContent = release_date || first_air_date;
+
+              $resultItem.append($resultImg, $resultName, $resultYear);
+
+              $resultsContainer.append($resultItem);
+            }
+          });
+        }
+      );
     }
   });
 
