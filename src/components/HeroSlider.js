78f8a808b32b8b1e3035b63feecd2120f8ab2e69
andrey <andreytorrente@gmail.com>
2022-05-19 20:59:29 -0600

    added trailer video to heroslider component

--- src/components/HeroSlider.js
+++ src/components/HeroSlider.js
@@ -1,13 +1,16 @@
-import { dataRequest } from "../helpers/Fetch";
-import { play, sliderControls } from "../helpers/icons";
-import { slideTo } from "../helpers/slideTo";
+import { fetchData } from "../helpers/fetchData";
+import {sliderControls } from "../helpers/icons";
 const { left, right } = sliderControls;
 
 export const HeroSlider = (mediaType) => {
   const $sliderContainer = document.createElement("DIV");
   $sliderContainer.classList.add("hero-slider-container");
-
-  dataRequest(
+  const $overlay = document.createElement('DIV');
+  $overlay.classList.add('video_overlay');
+ 
+  $overlay.insertAdjacentHTML('beforeend', `<i class="bi bi-x-circle-fill close-overlay"></i>`)
+  document.querySelector('#root').append($overlay)
+  fetchData(
     `https://api.themoviedb.org/3/trending/${mediaType}/day?api_key=${process.env.API_K}&language=en-US&page=1`,
 
     (data) => {
@@ -15,27 +18,53 @@ export const HeroSlider = (mediaType) => {
       $slider.classList.add("hero-slider");
 
       data.results.forEach((element) => {
-        const $contentInfoHeader = mediaType === "movie"? `${element.original_title}`: `${element.name}`;
-
-        const imageSrc = `https://image.tmdb.org/t/p/w1280${element.backdrop_path}`;
-
+        const $contentInfoHeader =
+          mediaType === "movie" ? `${element.original_title}` : `${element.name}`;
         const $content = document.createElement("DIV");
         $content.classList.add("hero-content-slider");
 
         $content.innerHTML = `
-        
-        <div class="hero-content-slider">
-          <img src="${imageSrc}" class="hero-content-img" />
+          <img src="https://image.tmdb.org/t/p/w1280${element.backdrop_path}" class="hero-content-img" />
           <div class="overlay-bg" style="opacity:1">
           
               <div class="hero-slider-info">
                 <h2>${$contentInfoHeader}</h2>
-                          ${play}
+                <i class="bi bi-play-circle-fill play" id=${element.id}></i>
               </div>      
-          </div>
-        </div>`;
+          </div>`;
 
         $slider.append($content);
+        
+
+        document.addEventListener('click', (e) =>{
+          if(e.target.id == element.id ){
+
+            $overlay.style.width = '100%'
+            document.body.style.overflowY = 'hidden'
+            document.querySelector('.loader').style.display = 'block'
+            const whatToSearch = location.hash.includes('#tv-shows') ? `https://api.themoviedb.org/3/tv/${element.id}/videos?api_key=${process.env.API_K}&language=en-US&page=1` : `https://api.themoviedb.org/3/movie/${element.id}/videos?api_key=${process.env.API_K}&language=en-US&page=1`
+
+            fetchData(
+              `${whatToSearch}`,
+              
+              (data)=>{
+                const video = document.createElement('DIV')
+                const search = ['Final Trailer', 'Trailer', 'Official Trailer','Official Trailer 1', 'Trailer 2', 'Teaser Trailer', 'Official Teaser', 'Official HBO Max Trailer','Main Trailer', 'Official Trailer | Netflix']
+
+                data.results.forEach((e)=>{
+                  if(search.includes(e.name) ){
+                    const w = screen.width; const h = screen.width< 648 ? screen.height/2 : screen.height-150
+                    video.innerHTML = `
+                    <iframe width=${w} height=${h} src="https://www.youtube.com/embed/${e.key}" title=${e.name} frameborder="0" allowfullscreen allowautoplay ></iframe>                   
+                   `           
+                  }
+                })
+                $overlay.append(video)
+                document.querySelector('.loader').style.display = 'none'               
+              }           
+              )
+          } 
+        })
       });
 
       const $sliderControls = document.createElement("DIV");
@@ -46,11 +75,21 @@ export const HeroSlider = (mediaType) => {
     }
   );
 
-  $sliderContainer.addEventListener("click", (evt) => {
+  document.addEventListener("click", (evt) => {
     const heroSlider = document.querySelector(".hero-slider");
     const screenWidth = screen.width;
 
-    slideTo(evt, heroSlider, screenWidth);
+    if (evt.target.matches(`.hero-slider-container .left`)) heroSlider.scrollBy(-screenWidth, 0);
+    if (evt.target.matches(`.hero-slider-container .right`)) heroSlider.scrollBy(screenWidth, 0);
+
+    if (evt.target.matches(`.close-overlay`)){
+
+      evt.target.parentElement.style.width = '0';
+      evt.target.nextElementSibling.remove()
+      document.body.style.overflowY = 'auto'
+
+    } 
+
   });
 
   return $sliderContainer;
