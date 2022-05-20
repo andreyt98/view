a6278848efe304fca4bdfd006a761e3798d15e66
andrey <andreytorrente@gmail.com>
2022-05-19 21:14:53 -0600

    added footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,26 @@
+import { Input } from "./Input.js";
+
 export const Footer = () => {
   const $footer = document.createElement("footer");
   $footer.classList.add("footer");
 
   $footer.innerHTML = `
         <div class="column">
-            <p>Get in touch</p>
+            <h3>Get in touch</h3>
+            <p>view@email.com</p>
         </div>
 
         <div class="column">
-            <p>Get in touch</p>
+            <h3>Connect</h3>
+            <a href=#>Twitter</a>
+            <a href=#>Instagram</a>
         </div>
 
         <div class="column">
-            <p>Get in touch</p>
+            <h3>Subscribe to newsletter</h3>
+            <form class="footer-form">
+                ${Input("email", "email@example.com", "Subscribe").innerHTML}
+            </form>
         </div>
     `;
 
