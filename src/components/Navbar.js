eb1209ecaaeb6743afc5f77839b6d21a82ecc007
andrey <andreytorrente@gmail.com>
2022-05-24 18:33:13 -0600

    update navbar component (take off of navbar results component)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,34 +15,37 @@ export const Navbar = () => {
 
     <form class="search-form"> 
     ${Input("search", "Search something", search).innerHTML}
-    </form>
-    
-    <div class="results-container"></div>
-    
+    </form> 
     `;
 
   window.addEventListener("scroll", () => {
     $navBar.classList.toggle("color-on-scroll", window.scrollY > 0);
   });
 
+  document.addEventListener('submit', (e) =>{
+    e.preventDefault();
+  })
+
   document.addEventListener("keyup", (e) => {
     if (e.target.matches(".search-form input")) {
+
       const $resultsContainer = document.querySelector(".results-container");
       $resultsContainer.style.display = "flex";
-
+      
       let query = document.querySelector(".search-form input").value;
       if (!query) $resultsContainer.style.display = "none";
-
+      
       $resultsContainer.innerHTML = "";
       fetchData(
         `https://api.themoviedb.org/3/search/multi?api_key=${process.env.API_K}&language=en-US&query=${query}&page=1&include_adult=false`,
 
         (data) => {
-          if (data.results.length <= 0)
+          if (data.results.length <= 0){            
             $resultsContainer.innerHTML = `There are no results for "${query}"`;
+          }
 
           data.results.forEach((element) => {
-            const { poster_path, media_type, release_date, first_air_date } = element;
+            const { poster_path, media_type, } = element;
             const resultName = element.name || element.original_title || element.original_name;
 
             if ((poster_path && resultName && media_type === "movie") || media_type === "tv") {
@@ -55,10 +58,8 @@ export const Navbar = () => {
               const $resultImg = document.createElement("IMG");
               $resultImg.src = `https://image.tmdb.org/t/p/w500${poster_path}`;
 
-              const $resultYear = document.createElement("P");
-              $resultYear.textContent = release_date || first_air_date;
 
-              $resultItem.append($resultImg, $resultName, $resultYear);
+              $resultItem.append($resultImg, $resultName);
 
               $resultsContainer.append($resultItem);
             }
