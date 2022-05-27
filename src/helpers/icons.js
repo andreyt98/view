972788252c3d83102e17ac364e2a101c9212e36d
andrey <andreytorrente@gmail.com>
2022-05-27 13:32:35 -0600

    delete unused loader from root and moved it to icons file

--- src/helpers/icons.js
+++ src/helpers/icons.js
@@ -5,3 +5,4 @@ export const sliderControls = {
 export const play = `<i class="bi bi-play-circle-fill play"></i>`;
 export const logo = `<i class="bi bi-play-btn"></i>`;
 export const search = `<i class="bi bi-search search-btn"></i>`;
+export const loader = `<img src="https://cutewallpaper.org/21/loading-gif-transparent-background/Free-Content-Discovery-Influencer-Marketing-Tool-Buzzsumo-.gif" class="loader"></img>`;
--- src/helpers/router.js
+++ src/helpers/router.js
@@ -3,15 +3,9 @@ import { Slider } from "../components/Slider.js";
 export const Router = () => {
   const { hash } = location;
   const root = document.getElementById("root");
-
-  const loader = document.createElement("img");
-  loader.classList.add("loader");
-  loader.src = `https://cutewallpaper.org/21/loading-gif-transparent-background/Free-Content-Discovery-Influencer-Marketing-Tool-Buzzsumo-.gif`;
-
   if (!hash || hash === "#home" || hash === "#movies") {
     document.querySelector('a[href="#movies"]').style.borderBottom = "1px solid #e95f0f";
     root.append(
-      loader,
       HeroSlider("movie"),
       Slider("| Popular Movies", "movie", "popular"),
       Slider("| Upcoming Movies", "movie", "upcoming")
@@ -20,7 +14,6 @@ export const Router = () => {
     document.querySelector('a[href="#tv-shows"]').style.borderBottom = "1px solid #e95f0f";
 
     root.append(
-      loader,
       HeroSlider("tv"),
       Slider("| Popular TV Shows", "tv", "popular"),
       Slider("| Top Rated TV Shows", "tv", "top_rated")
