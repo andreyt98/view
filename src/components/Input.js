088b0406f84b8a6d5cb0967656de1917cc9a6296
andrey <andreytorrente@gmail.com>
2022-05-26 18:27:56 -0600

    add input component

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,10 +1,11 @@
-export const Input = (inputType, placeholder, buttonText) => {
+export const Input = (inputType, placeholder, hasButton, buttonText) => {
   const $input = document.createElement("DIV");
 
   $input.innerHTML = `
     <input type =${inputType} placeholder="${placeholder}" autocomplete="off">
-    <button type="submit" >${buttonText}</button>
-
     `;
+    if(hasButton){
+      $input.insertAdjacentHTML('beforeend',`<button type="submit">${buttonText}</button>`)
+    }
   return $input;
 };
