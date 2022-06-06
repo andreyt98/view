000bbab73298713c9b8562fe0b9f12e42b47e699
andrey <andreytorrente@gmail.com>
2022-06-06 13:45:38 -0600

    change footer background

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,22 +5,26 @@ export const Footer = () => {
   $footer.classList.add("footer");
 
   $footer.innerHTML = `
-        <div class="column">
-            <h3>Get in touch</h3>
-            <p>view@email.com</p>
-        </div>
 
-        <div class="column">
-            <h3>Connect</h3>
-            <a href=#>Twitter</a>
-            <a href=#>Instagram</a>
-        </div>
+        <div class="footer-content">   
+            <div class="column">
+                <h3>Get in touch</h3>
+                <p>view@email.com</p>
+            </div>
 
-        <div class="column">
-            <h3>Subscribe to newsletter</h3>
-            <form class="footer-form">
-                ${Input("email", "email@example.com", true,"Subscribe").innerHTML}
-            </form>
+            <div class="column">
+                <h3>Connect</h3>
+                <a href=#>Twitter</a>
+                <a href=#>Instagram</a>
+            </div>
+
+            <div class="column">
+                <h3>Subscribe to newsletter</h3>
+                <form class="footer-form">
+                    ${Input("email", "email@example.com", true,"Subscribe").innerHTML}
+                </form>
+            </div>
+
         </div>
     `;
 
