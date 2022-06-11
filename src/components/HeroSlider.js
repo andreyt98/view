f8f36aa5ba337b17eb3fb0ff90217f54e8f42d3a
andrey <andreytorrente@gmail.com>
2022-06-10 19:03:58 -0600

    add media info to heroSlider and change view slider cards hover

--- src/components/HeroSlider.js
+++ src/components/HeroSlider.js
@@ -9,6 +9,10 @@ export const HeroSlider = (mediaType) => {
   const $sliderContainer = document.createElement("DIV");
   $sliderContainer.classList.add("hero-slider-container");
 
+  const $sliderControls = document.createElement("DIV");
+  $sliderControls.classList.add("controls");
+  $sliderContainer.appendChild($sliderControls)
+
   fetchData(
     `https://api.themoviedb.org/3/trending/${mediaType}/day?api_key=${process.env.API_K}&language=${lang}&page=1`,
 
@@ -16,39 +20,48 @@ export const HeroSlider = (mediaType) => {
       const $slider = document.createElement("DIV");
       $slider.classList.add("hero-slider");
 
-      data.results.forEach((element) => {
-        const $contentInfoHeader =
-          mediaType === "movie" ? `${element.original_title}` : `${element.name}`;
-
+      data.results.slice(0,10).forEach((element) => {
+        const $title =
+        mediaType === "movie" ? `${element.original_title}` : `${element.name}`;
+        const $rate = document.createElement("P");
+        $rate.textContent = element.vote_average;
         const $content = document.createElement("DIV");
         $content.classList.add("hero-content-slider");
 
         $content.innerHTML = `
           <img src="https://image.tmdb.org/t/p/w1280${element.backdrop_path}" class="hero-content-img" />
-          <div class="overlay" style="opacity:1">
-          
-              <div class="hero-slider-info">
-                <h2>${$contentInfoHeader}</h2>
-                <i class="bi bi-play-circle-fill play" id=${element.id}></i>
-              </div>      
+          <div class="overlay" style="opacity:1">     
+            <div class="hero-slider-info">
+              <h2 style="font-size:2rem">${$title}</h2>
+
+              <div>
+                <i class="bi bi-star-fill stars"></i>
+                <i class="bi bi-star-fill stars"></i>
+                <i class="bi bi-star-fill stars"></i>
+                <i class="bi bi-star-fill stars"></i>
+                <i class="bi bi-star-half stars"></i> <span style="color:lightsteelblue">(${$rate.textContent})</span>
+              </div>
+
+              <button class="primary-btn play" id=${element.id}>
+                <i class="bi bi-play-circle-fill "></i>
+                Watch Trailer
+              </button> 
+            </div>
+                        
           </div>`;
-
-        $slider.append($content);
-
+                  
+        $slider.append($content);    
+        
         document.addEventListener("click", (e) => {
-          
           if (e.target.matches(".hero-slider .play") && e.target.id == element.id) {
             renderTrailer(element);
           }
-
         });
       });
-
-      const $sliderControls = document.createElement("DIV");
-      $sliderControls.classList.add("controls");
+ 
       $sliderControls.innerHTML = `${left} ${right}`;
 
-      $sliderContainer.append($slider, $sliderControls);
+      $sliderContainer.append($slider,$sliderControls );
     }
   );
 
--- src/components/Slider.js
+++ src/components/Slider.js
@@ -4,52 +4,43 @@ import { fetchData } from "../helpers/fetchData";
 import { renderTrailer } from "../helpers/renderTrailer.js";
 import { lang } from "../App";
 
-export const Slider = (header, mediaType, category) => {
+export const Slider = (header, mediaType, typeOfSearch, category) => {
   const $sliderContainer = document.createElement("DIV");
   $sliderContainer.classList.add("slider-container");
 
+  let searchIndex =1;
+
   fetchData(
-    `https://api.themoviedb.org/3/${mediaType}/${category}?api_key=${process.env.API_K}&language=${lang}&page=1&include_adult=false`,
+    `https://api.themoviedb.org/3/${typeOfSearch}/${category}?api_key=${process.env.API_K}&language=${lang}&page=1&include_adult=false`,
 
     (data) => {
       $sliderContainer.innerHTML = `
           <div class="slider-header">
-            <h2>${header}</h2>
+            <h2>${header} <span>${mediaType} </span> </h2>
             <div class="controls">${left} ${right}</div>
           </div>
       `;
 
       const $slider = document.createElement("DIV");
       $slider.classList.add("slider");
+      const $moreContent = document.createElement("DIV");
+      $moreContent.classList.add("more");
 
       data.results.forEach((element) => {
-        const $content = document.createElement("DIV");
-        $content.classList.add("content");
-        let mediaName = mediaType === "movie" ? element.original_title : element.name;
-        
-        const $rate = document.createElement("P");
-        $rate.textContent = element.vote_average;
-        const col = element.vote_average < 7 ? "red" : element.vote_average >= 7 && element.vote_average < 7.8 ? "yellow" : "green";
+      
+        const content = loadSliderContent(element,typeOfSearch);
+        $slider.append(content);
 
-        $content.innerHTML = `
-        <img src=https://image.tmdb.org/t/p/w500${element.poster_path} class="content-img"></img>
-          <div class="overlay">
-            <p>${mediaName}</p>
-            <p style=color:${col}> ${$rate.textContent}</p>
-            <button class="primary-btn" id=${element.id}>View trailer</button>    
-          </div> 
+        $moreContent.innerHTML = `
+          <button class="primary-btn more">Load more</button>
         `;
-        $slider.append($content);
 
-        document.addEventListener("click", (e) => {
+        $slider.appendChild($moreContent);
+        $sliderContainer.append($slider);
 
-          if (e.target.matches(".slider button") && e.target.id == element.id) {
-            renderTrailer(element,lang);
-          }
-          
-        });
+        loadTrailerOnClick(element);
       });
-      $sliderContainer.append($slider);
+       
     }
   );
 
@@ -65,7 +56,59 @@ export const Slider = (header, mediaType, category) => {
       overlay.remove();
       document.body.style.overflowY = "auto";
     }
+
+    if (evt.target.matches(".more") ) {
+
+      fetchData(
+        `https://api.themoviedb.org/3/${typeOfSearch}/${category}?api_key=${process.env.API_K}&language=${lang}&page=${++searchIndex}&include_adult=false`,
+        
+        (data) => {
+          const slider = evt.target.parentElement.parentElement;
+          const $moreContent = document.createElement("DIV");
+          $moreContent.classList.add("more");
+          
+          data.results.forEach((element) => {
+            const content = loadSliderContent(element,typeOfSearch);
+            slider.append(content)
+
+            $moreContent.innerHTML = `
+            <button class="primary-btn more">Load more</button>
+            `;
+            loadTrailerOnClick(element);
+         
+          });
+
+          slider.appendChild($moreContent);
+          
+          const loadMoreButton = evt.target.parentElement;
+          loadMoreButton.remove()
+        }
+      );
+    }
+
   });
 
   return $sliderContainer;
 };
+
+function loadSliderContent(element){
+   const $content = document.createElement("DIV");
+    $content.classList.add("content");
+
+    $content.innerHTML = `
+    <img src=https://image.tmdb.org/t/p/w500${element.poster_path} class="content-img"></img>
+      <div class="overlay">
+      <i class="bi bi-play-circle play" id=${element.id}></i>
+      </div> 
+
+      `; 
+  return $content;
+} 
+
+function loadTrailerOnClick(element){
+  document.addEventListener("click", (e) => {                  
+    if (e.target.matches(".slider .play") && e.target.id == element.id) {
+      renderTrailer(element,lang);
+    }          
+  });
+}
