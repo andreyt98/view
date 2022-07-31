c3966abbae8cd57f8230baba85f3f7d6d85283d1
andrey <andreytorrente@gmail.com>
2022-07-31 09:06:20 -0600

    add responsive search element for phone screens

--- src/components/ResultsContainer.js
+++ src/components/ResultsContainer.js
@@ -1,14 +1,18 @@
 export const resultsContainer = () => {
     const $resultsContainer = document.createElement('DIV');
     $resultsContainer.classList.add('results-container');
+    const $contenedor = document.createElement('DIV');
+    $contenedor.classList.add('contenedor');
+
+    $contenedor.append($resultsContainer)
 
     document.addEventListener('click', (evt) =>{
         const toMatch = ['.search-form input','.result-item']
-        if(!evt.target.matches(toMatch)){
-            $resultsContainer.style.display = 'none'
-            document.querySelector(".search-form input").value = '';
-        }
+        // if(!evt.target.matches(toMatch)){
+        //     $resultsContainer.style.display = 'none'
+        //     document.querySelector(".search-form input").value = '';
+        // }
     })
 
-    return $resultsContainer;
+    return $contenedor;
 }
